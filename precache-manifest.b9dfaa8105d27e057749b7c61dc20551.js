dc2d3a26185828afa62b46355d073f407951d2dd
Gokay Arpaci <gokayarpaci@gmail.com>
2019-07-02 06:22:15 +0300

    new deployment

---
+++ precache-manifest.b9dfaa8105d27e057749b7c61dc20551.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7812baace544527cbb7f0db6c8fb65a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52a0a95e18a088482004",
+    "url": "/static/css/main.147ad574.chunk.css"
+  },
+  {
+    "revision": "22a0e590f600b7598fbb",
+    "url": "/static/js/2.8b94e47a.chunk.js"
+  },
+  {
+    "revision": "52a0a95e18a088482004",
+    "url": "/static/js/main.8eb08e32.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
