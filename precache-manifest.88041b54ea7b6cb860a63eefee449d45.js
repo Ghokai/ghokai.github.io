922261a3780772d9bdf361a842c0e0aafc702757
Gokay Arpaci <gokayarpaci@gmail.com>
2019-07-03 23:30:30 +0300

    favourite movies app

---
+++ precache-manifest.88041b54ea7b6cb860a63eefee449d45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1228e35eefbb7c21e79fb6fc1a73979",
+    "url": "/index.html"
+  },
+  {
+    "revision": "853a2dfcfda7380cfbb5",
+    "url": "/static/css/2.41a373cd.chunk.css"
+  },
+  {
+    "revision": "4c8af33df3367f1d9510",
+    "url": "/static/css/main.af30da5a.chunk.css"
+  },
+  {
+    "revision": "853a2dfcfda7380cfbb5",
+    "url": "/static/js/2.24c96da0.chunk.js"
+  },
+  {
+    "revision": "4c8af33df3367f1d9510",
+    "url": "/static/js/main.7726a3a6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
